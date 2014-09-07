5e928c8074a503eb17f0dac80662eb7e07d95635
brent <brent.arata@gmail.com>
2014-09-07 13:58:48 -0700

    Forgot to add the reference to firebase integration for the index.html, should be in there now

--- app/src/main.js
+++ app/src/main.js
@@ -4,9 +4,11 @@ define(function(require, exports, module) {
     // import dependencies
     var Engine = require('famous/core/Engine');
     var AppView = require('views/AppView');
+	var PageController = require('controllers/PageController');
 	// create the main context
-    var mainContext = Engine.createContext();
 	
+	var pageController = new PageController();
+    var mainContext = Engine.createContext();
 	var appView = new AppView();
 	mainContext.add(appView);
 	
