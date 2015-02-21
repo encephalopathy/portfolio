a31490fd0afa86a61ea74250c022fefac74e638b
brent <brent.arata@gmail.com>
2015-02-20 16:50:06 -0800

    Got rid of some old scroll view code and some debug statements

--- app/src/views/Gallery.js
+++ app/src/views/Gallery.js
@@ -27,10 +27,6 @@ define(function(require, exports, module) {
         View.apply(this, arguments);
 		_createLightBox.call(this);
 		_createGrid.call(this);
-		
-		//_createBackground.call(this);
-		//_createScrollView.call(this);
-		_setListeners.call(this);
     }
 
     Gallery.prototype = Object.create(View.prototype);
@@ -47,7 +43,7 @@ define(function(require, exports, module) {
 				 5 : 'content/images/Red_Sea_take_1.png',
 				 6 : 'content/images/Red_Sea_take_2.png'
 		},
-		offScreenOffsetX : 1500,
+		offScreenOffsetX : 2000,
 		offScreenOffsetY : 0,
 		size : [ window.innerWidth, window.innerHeight],
 		orderNumber : 4,
@@ -64,8 +60,6 @@ define(function(require, exports, module) {
 		var scrollview = new ScrollView();
 		var contextSize = this.options.size;
 		scrollview.sequenceFrom(scrollViews);
-		scrollview.offsetY = 0;
-		this.offsetY = 0;
 		var gridCells = [];
 		
 		var grid = new GridLayout();
@@ -115,7 +109,6 @@ define(function(require, exports, module) {
 				  center[0] = center[0] - window.innerWidth / 2;
 				  center[1] = center[1] - window.innerHeight / 2;
 				  console.log('LightBox inTransformCenter: ' + center);
-				  console.log('this.offsetY when lightbox is shown: ' + this.offsetY);
 				  var finalTransform = null;
 				  var interuptationTransform = null;
 				  var scaleFactor = [size[0] / window.innerWidth, size[1] / window.innerHeight];
@@ -126,34 +119,45 @@ define(function(require, exports, module) {
 				  console.log('Scale: ' + interuptationTransform.scale);
 				  
 				  finalTransform = Transform.multiply(Transform.translate(0, center[1] , 1), scaleAmount);
-				  //finalTransform = Transform.multiply(Transform.translate(0, viewTemp.offsetY, 1), finalTransform);
 				  interuptationTransform = Transform.interpret(finalTransform);
 				  console.log('Translation: ' + interuptationTransform.translate);
 				  lightBox.options.inTransform = finalTransform;
 				  
-				  lightBox.options.showTransform = Transform.translate(0, center[1], 0);
+				  console.log('Center y: ' + center[1]);
+				  
+				  var showPosY = center[1];
+				  
+				  //Hack for now until I get the light box to fit within the screen bounds wherever you click.
+				  if (showPosY < 0) showPosY = 0;
+				  //if (showPosY > size[1]) showPosY = showPosY - cellHeight / 2;
+				  
+				  lightBox.options.showTransform = Transform.translate(0, showPosY, 0);
 				  lightBox.options.outTransform = Transform.multiply(Transform.translate(0, center[1], 0), Transform.scale(0.001, 0.001, 0.001))
-				  var showTransform = Transform.interpret(lightBox.options.outTransform);
-				  console.log('OUT TRANFORM: ')
+				  var showTransform = Transform.interpret(lightBox.options.showTransform);
+				  console.log('SHOW	 TRANFORM: ')
 				  console.log('Translation: ' + showTransform.translate);
 				  console.log('Scale: ' + showTransform.scale);
 				  console.log('Rotation: ' + showTransform.rotation);
 				  this.shown = true;
-				  viewTemp.disableScrollView = true;
-				  lightBox.showing = true;
 				  lightBox.show(this);
 		  	  }
 			  else {
 				  this.shown = false;
-				  viewTemp.disableScrollView = false;
-				  lightBox.showing = false;
 				  lightBox.hide(this);
 			  }
 		  });
 	  	}
 	  
 	  scrollViews.push(grid.node);
-	  this.add(scrollview);
+	  
+	  var scrollViewTransformModifier = new StateModifier({
+		  transform : Transform.translate(this.options.offScreenOffset, window.innerHeight * 0.5, -5),
+		  origin : [0.5, 0.5]
+	  });
+	  
+	  this.add(grid.mod);
+	  this.add(scrollViewTransformModifier).add(scrollview);
+	  this.scrollViewTransform = scrollViewTransformModifier;
 	  this.scrollView = scrollview;
 	}
 	
@@ -164,141 +168,21 @@ define(function(require, exports, module) {
 			overlap : true
 		});
 		
-		lightBox.mod = new StateModifier({
-			origin : [0.5,0.5]
-		});
 		
-		//lightBox.mod.add(lightBox);
-		//this.lightBox = lightBox;
 		this.lightBox = lightBox;
-		this.add(lightBox.mod).add(lightBox);
-	}
-	
-	function _createScrollView() {
-		
-		var description = this.options.images[0];
-		var scrollView = new ScrollView({
-		});
-		
-		var scrollModifier = new StateModifier({
-			size : [750, 750],
-			origin : [0.5, 0.5]
-		});
-		
-		var scrollViewTransformModifier = new StateModifier({
-			origin : [0.5, 0.5],
-			size : [250, 250],
-			transform : Transform.translate(this.options.offScreenOffsetX, 0, -3)
-		}); 
-		
-		this.scrollView = scrollView;
-		this.add(scrollViewTransformModifier).add(scrollView);
-		
-		var surfaces = [];
-		var surfaceModifiers = [];
-		var modifiers = [];
-		scrollView.sequenceFrom(surfaces);
-		
-		var draggable = new Draggable({
-			xRange : [-220, 220],
-			yRange : [0, 0]
-		})
-		
-		for (var i = 0; i < 7; ++i) {
-			image = new ImageSurface({
-				//size : [500, 500],
-				content : this.options.images[i]
-			});
-			
-			draggable = new Draggable({
-						xRange : [-220, 220],
-						yRange : [50, 50]
-			})
-			
-			var renderNode = new RenderNode(draggable);
-			
-			var modifier = new StateModifier({
-			});
-			
-			renderNode.add(modifier).add(image);
-			image.pipe(renderNode);
-			
-			image.pipe(scrollView);
-			surfaces.push(renderNode);
-			surfaceModifiers.push(modifier);
-		}
-		
-		this.imageModifiers = surfaceModifiers;
-		this.scrollViewTransformModifier = scrollViewTransformModifier;
-	}
-	
-	function _createBackground() {
-		/*var blackBackground = new Surface({
-			size : [window.innerWidth, window.innerHeight],
-			properties : {
-				backgroundColor : 'black'
-			}
-		});
-		
-		var transformModifier = new StateModifier({
-			origin : [0.5, 0.5],
-			opacity : 0.25
-		});
-		
-		this.add(transformModifier).add(blackBackground);
-		
-		this.backgroundModifier = transformModifier;*/
-	}
-	
-	function _setListeners() {
-		this.scrollView.sync.on("start", _onTouchStart.bind(this));
-		this.scrollView.sync.on("update", _update.bind(this));
-		this.scrollView.sync.on("end", _onTouchEnd.bind(this));
-	}
-	
-	function _onTouchStart(event) {
-		//this.scrollView.getPosition()
-		console.log('On Touch start event')
-		console.log(event);
-	}
-	
-	function _update(event) {
-		console.log(event);
-		var lightBoxTransformContainer = this.lightBox.options.__proto__;
-		var lightBox = this.lightBox;
-		var lightBoxTransformInfo;
-		//Show the lightbox and translate the lightbox down by w/e units the user scrolls by.
-		if (lightBox.showing) {
-			lightBoxTransformInfo = Transform.interpret(lightBoxTransformContainer.showTransform);
-			var showTranslation = lightBoxTransformInfo.translate;
-			console.log('Moving visible lightbox');
-			console.log('Scale info: ' + lightBoxTransformInfo.scale);
-			console.log('Rotation info: ' + lightBoxTransformInfo.rotate);
-			console.log('Translation: ' + showTranslation);
-			lightBoxTransformContainer.showTransform = Transform.translate(Transform.translate(showTranslation[0], showTranslation[1] + this.position, showTranslation[2]));
-		}
-		
-		if (this.disableScrollView) {
-			this.scollView_earlyEnd = true;
-		}
-	}
-	function _onTouchEnd(event) {
+		this.add(lightBox);
 	}
 	
 	Gallery.prototype.transitionIn = function(event, tabName) {
 		console.log('Transition In');
 		var centerX = window.innerWidth * 0.5;
 		var centerY = window.innerHeight * 0.5;
-		/*this.scrollViewTransformModifier.setTransform(
-			Transform.translate(centerX, centerY, -3), {
-			duration : 1000, curve : 'easeInOut'
-		});
 		
-		this.backgroundModifier.setTransform(
-			Transform.translate(centerX, centerY, -5), {
+		this.scrollViewTransform.setTransform(
+			Transform.translate(centerX, centerY, -5), { 
 				duration : 1000, curve : 'easeInOut'
 			}
-		)*/
+		)
 	}
 	
 	Gallery.prototype.transitionOut = function(oldTabName, orderNumber) {
@@ -308,23 +192,14 @@ define(function(require, exports, module) {
 		var centerY = window.innerHeight * 0.5;
 		if (this.options.orderNumber < orderNumber) {
 			
-			this.scrollViewTransformModifier.setTransform(
+			this.scrollViewModifier.setTransform(
 				Transform.translate(centerX -this.options.offScreenOffsetX, centerY, -5), {
 					duration : 1000, curve : 'easeInOut'
 				}
 			);
-			
-			this.backgroundModifier.setTransform(Transform.translate(centerX -this.options.offScreenOffsetX, centerY, -5), {
-					duration : 1000, curve : 'easeInOut'
-			})
 		}
 		else {
-			this.scrollViewTransformModifier.setTransform(
-				Transform.translate(this.options.offScreenOffsetX, 0, -5), {
-					duration : 1000, curve : 'easeInOut'
-			});
-			
-			this.backgroundModifier.setTransform(
+			this.scrollViewModifier.setTransform(
 				Transform.translate(this.options.offScreenOffsetX, 0, -5), {
 					duration : 1000, curve : 'easeInOut'
 			});
