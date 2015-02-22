75d2d5e3225a4bad1f465c4daea53a7fed68039a
brent <brent.arata@gmail.com>
2015-02-21 21:03:10 -0800

    Adding the Procfile for Heroku deployment

--- app/src/server.js
+++ app/src/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var index = fs.readFileSync('index.html');
+var index = fs.readFileSync('../index.html');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
