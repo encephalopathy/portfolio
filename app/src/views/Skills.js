4378bf9ae58b1c137e3df0cc1f37a6932b7f0753
brent <brent.arata@gmail.com>
2015-05-06 19:45:57 -0700

    Added place holder images and text fields for the home page

--- app/src/views/Skills.js
+++ app/src/views/Skills.js
@@ -16,14 +16,15 @@ define(function(require, exports, module) {
         View.apply(this, arguments);
 		
 		_createBackground.call(this);
+		_createLogos.call(this);
 		
     }
 	
 	function _createBackground() {
 		var backgroundSurface = new Surface({
-			size : [undefined, 250],
+			size : [undefined, undefined],
 			properties : {
-				backgroundColor : 'white'
+				backgroundColor : 'blue'
 			}
 		});
 		
@@ -34,21 +35,70 @@ define(function(require, exports, module) {
 	}
 	
 	function _createLogos() {
-		/*var programmingLogo = new ImageSurface({
-			content : ''
+		var programmingLogo = new ImageSurface({
+			content : 'content/images/Programming_Logo_One.png',
+			size : [400, 400]
 		});
 		
 		var designLogo = new ImageSurface({
-			content : ''
+			content : 'content/images/paint_brush_wooden_figure_logo.jpg',
+			size : [400, 400]
 		});
+		
+		var textSize = this.options.textSize
+		var programmingText = new Surface({
+			color: 'black',
+			content : 'Programming',
+			size : [textSize, textSize],
+            fontFamily: 'AvenirNextCondensed-DemiBold',
+            fontSize: this.options.fontSize + 'px',
+            textTransform: 'uppercase',
+            pointerEvents : 'none',
+			textAlign : 'center'
+			
+		});
+		
+		var designText = new Surface({
+			color: 'black',
+			content : 'Art & Design',
+			size : [textSize, textSize],
+            fontFamily: 'AvenirNextCondensed-DemiBold',
+            fontSize: this.options.fontSize + 'px',
+            textTransform: 'uppercase',
+            pointerEvents : 'none',
+			textAlign : 'center'
+		});
+		
+		
+		var centerX = window.innerWidth / 3;
+		var centerDisplacement = this.options.centerDisplacement;
 		
 		var programmingTransform = new StateModifier({
-			
+			transform : Transform.translate(centerX - centerDisplacement, 0, 7)
 		});
 		
+		var programmingTitleTransform = new StateModifier({
+			transform : Transform.translate(centerX - textSize / 2, 400, 7)
+		})
+		
 		var designTransform = new StateModifier({
-			
-		});*/
+			transform : Transform.translate(centerX + centerDisplacement, 0, 7)
+		});
+		
+		var designTextTransform = new StateModifier({
+			transform : Transform.translate(centerX + centerDisplacement, 400, 7)
+		})
+		
+		//Add all the icons to the view.
+		this.add(programmingTransform).add(programmingLogo);
+		this.add(programmingTitleTransform).add(programmingText);
+		this.add(designTransform).add(designLogo);
+		this.add(designTextTransform).add(designText);
+		
+		programmingLogo.pipe(this._eventOutput);
+		designLogo.pipe(this._eventOutput);
+		programmingText.pipe(this._eventOutput);
+		designText.pipe(this._eventOutput);
 	}
 	
 	Skills.prototype.transitionIn = function() {
@@ -64,7 +114,9 @@ define(function(require, exports, module) {
     Skills.prototype.constructor = Skills;
 
     Skills.DEFAULT_OPTIONS = {
-		
+		fontSize : 50,
+		centerDisplacement : 250,
+		textSize : 200
     };
 
     module.exports = Skills;
