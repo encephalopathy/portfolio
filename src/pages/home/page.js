96b6ecd680df9e1767d970f8d4d0bee850d0d336
brent <brent.arata@gmail.com>
2016-09-05 15:25:03 -0700

    Added stock placeholder backgrounds for the gallery and the landing section

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -1,23 +1,43 @@
 import React from "react";
 import styles from "./style.css";
-
-
+//import imgContent from "./content/images";
+//TODO: CDN the images from somewhere.
 export default class HomePage extends React.Component {
   render() {
     return (
-      <div className={styles.content}>
-        <h1>Home Page</h1>
-        <p className={styles.welcomeText}>Thanks for joining!</p>
-      </div>
-      <div className={styles.content}>
-       <h1>Gallery</h1>
-      </div>
-      <div className={styles.content}>
-       <h1>Services</h1>
-      </div>
-      <div className={styles.content}>
-       <h1>Contact</h1>
+      <div>
+      <section className={styles.mainBg}>
+        <div className={styles.title}>Brent Arata</div>
+      </section>
+      <section className={styles.galleryBg}>
+        <div className={styles.container}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </section>
+      <section>
+      </section>
       </div>
     );
   }
 }
+
+class Header extends React.Component {
+  //TODO: Replace image and text alignment with a video.
+  render() {
+    return (
+      <section className={styles.mainBg}>
+        <div className={styles.title}>Brent Arata</div>
+      </section>
+    )
+  }
+}
+
+class BackgroundImage extends React.Component {
+  render() {
+    return (
+        <img src={this.props.src} className={styles.container}></img>
+    )
+  }
+};
