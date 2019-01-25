ceb2064b72da469fb424310c3aedb1ba560692ac
brent <brent.arata@gmail.com>
2019-01-24 20:07:55 -0800

    Fixed the issue where the react-slick carousels wouldn't work also fixed some npm auditing issues with upgrading gatsby

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Slider from "react-slick";
 
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+
+
 const settings = {
   // dots: true,
   infinite: true,
@@ -44,38 +48,6 @@ const Testimonials = () => (
             </Slider>
           </ul>
         </div>
-        {/* <div className="ten columns flex-container">
-          <div className="flexslider">
-            <ul className="slides">
-              <li>
-                <blockquote>
-                  <p>
-                    Your work is going to fill a large part of your life, and
-                    the only way to be truly satisfied is to do what you believe
-                    is great work. And the only way to do great work is to love
-                    what you do. If you haven't found it yet, keep looking.
-                    Don't settle. As with all matters of the heart, you'll know
-                    when you find it.
-                  </p>
-                  <cite>Steve Jobs</cite>
-                </blockquote>
-              </li>
-
-              <li>
-                <blockquote>
-                  <p>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida
-                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                    quis bibendum auctor, nisi elit consequat ipsum, nec
-                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
-                    vulputate cursus a sit amet mauris.
-                  </p>
-                  <cite>Mr. Adobe</cite>
-                </blockquote>
-              </li>
-            </ul>
-          </div>
-        </div> */}
       </div>
     </div>
   </section>
