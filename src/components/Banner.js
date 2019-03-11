f0674457ac80a1b620f881e2b9ea25a7173ae6c6
Brent Arata <brent.arata@gmail.com>
2019-03-11 00:18:34 -0700

    Changed my description content

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,21 +6,10 @@ const Banner = () => (
     <div className="banner-text">
       <h1 className="responsive-headline">I'm Brent Arata.</h1>
       <h3>
-        I'm a California based <span>software engineer</span>, &nbsp;
-        <span>game developer</span> and <span>tinkerer</span> creating awesome
-        and effective visual and engaging experiences for companies of all sizes around the
-        globe. Let's
-        <a className="smoothscroll" href="#about">
-          {" "}
-          start scrolling
-        </a>{" "}
-        and learn more
-        <a className="smoothscroll" href="#about">
-          {" "}
-          about me
-        </a>.
+        I'm a California based software engineer,
+        game developer and tinkerer creating awesome
+        and effective visual and engaging experiences for companies of all sizes.
       </h3>
-      <hr />
     </div>
   </div>
 );
