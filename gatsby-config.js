332cc8032b378e8ee2e2d0c314311fb66cca0339
Brent Arata <brent.arata@gmail.com>
2019-03-12 23:09:09 -0700

    Added favicon & changed description text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Ceevee",
-    author: "Aman Mittal",
-    description: "A Gatsby.js Starter based on Ceevee by Styleshout",
+    title: "Brent Arata - Software Engineer",
+    author: "Brent Arata",
+    description: "A Gatsby.js Creation",
     galleryInfo: [
       {
         title: 'CLIMB!',
@@ -24,6 +24,39 @@ module.exports = {
   },
   pathPrefix: "/",
   plugins: [
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/assets/favicon/favicon.png",
+
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
