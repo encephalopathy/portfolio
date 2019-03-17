2e6b2217da2e1c7e37aee2c7de9d4fa3a61ce2e1
Brent Arata <brent.arata@gmail.com>
2019-03-17 07:59:46 -0700

    Added otoshi's testimonial

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -27,14 +27,10 @@ const Testimonials = () => (
               <li>
                 <blockquote>
                   <p>
-                    Your work is going to fill a large part of your life, and
-                    the only way to be truly satisfied is to do what you believe
-                    is great work. And the only way to do great work is to love
-                    what you do. If you haven't found it yet, keep looking.
-                    Don't settle. As with all matters of the heart, you'll know
-                    when you find it.
+                    An adapdatable and dedicated problem who excels at technical challenges and
+                    keeps morale high.
                   </p>
-                  <cite>Steve Jobs</cite>
+                  <cite>Kevin Otoshi - Otoshi Games</cite>
                 </blockquote>
               </li>
               <li>
